e27eb67f1b3d4cb267f63eb54fb934cf123b4eb2
Arturas Leonovas <arthur@gmail.com>
2016-03-10 20:35:44 +0000

    massive upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var codepre = function() {
+  var defaults = {
+    codeContainer: '.js-markup',
+    htmlbtn: '.js-button',
+    closeIcon: '.js-close'
+  };
+  return {
+    init: function (configuration) {
+      if(typeof configuration === 'undefined')
+        configuration = {};
+      config = $.extend(true, defaults, configuration);
+      // dropdown.showHideDropdown();
+      codepre.toggle();
+      codepre.closeCode();
+    },
+    toggle: function() {
+      // $(defaults.htmlbtn).on('click', function() {
+      //   var tog = $(this).attr("data-class");
+      //   $("." + tog).slideToggle("fast");
+
+      $(defaults.htmlbtn).on('click', function() {
+
+        var tog = $(this).attr("data-class");
+        var toggleVal = $("." + tog);
+
+        $(defaults.codeContainer).filter('.active').each(function() {
+          $(this).removeClass('active').fadeOut('fast');
+        });
+
+        if($(toggleVal).hasClass('active')) {
+            $(toggleVal).removeClass('active').fadeOut('fast');
+        } else {
+          $(toggleVal).addClass('active').fadeIn('fast');
+        }
+      });
+    },
+    closeCode: function() {
+      $(defaults.closeIcon).on('click', function(){
+        $(defaults.codeContainer).removeClass('active').fadeOut('fast');
+      });
+    }
+
+  };
+}();
